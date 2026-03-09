a1fddc11ab7408f88597e4fb2cad8a0c67bc6c7d
killi <kllnzhou@gmail.com>
2026-03-10 00:08:18 +0100

    ansible k3s lb bash to ansible native wip

--- ansible/scripts/k3s-node.install.sh
+++
@@ -1,4 +0,0 @@
-sh -c "$(wget https://raw.githubusercontent.com/zhou-efr/tdzhouse/master/ansible/scripts/debian/proxy-setup.install.sh -O -)"
-sh -c "$(wget https://raw.githubusercontent.com/zhou-efr/tdzhouse/master/ansible/scripts/debian/quick-debian.install.sh -O -)"
-
-curl -sfL https://get.k3s.io | sh -s - server --cluster-init --tls-san=10.1.1.49

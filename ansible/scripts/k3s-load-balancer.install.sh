8b0a6d1866755c45038020d77dbfc4f9f5fab6ee
killi <kllnzhou@gmail.com>
2026-03-09 23:58:41 +0100

    ansible k3s lb bash to ansible native wip

--- ansible/scripts/k3s-load-balancer.install.sh
+++
@@ -1,7 +0,0 @@
-sh -c "$(wget https://raw.githubusercontent.com/zhou-efr/tdzhouse/master/ansible/scripts/debian/proxy-setup.install.sh -O -)"
-sh -c "$(wget https://raw.githubusercontent.com/zhou-efr/tdzhouse/master/ansible/scripts/debian/quick-debian.install.sh -O -)"
-
-apt install nginx -y
-rm /etc/nginx/nginx.conf
-wget https://raw.githubusercontent.com/zhou-efr/tdzhouse/master/ansible/config/nginx/nginx.conf -P /etc/nginx/
-systemctl restart nginx

baede828de01364324a62ae51d307dddca823170
killi <kllnzhou@gmail.com>
2026-04-01 22:28:32 +0200

    clean repo

--- sub_scripts/0trust.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-repository=$1
-branch=$2
-inventory_hostname=$3
-sudo /usr/bin/wget https://raw.githubusercontent.com$repository/$branch/sub_scripts/0trust/anti_lock -O /etc/sudoers.d/anti_lock
-sudo /usr/bin/wget https://raw.githubusercontent.com$repository/$branch/sub_scripts/0trust/$inventory_hostname\_zt -O /etc/sudoers.d/ansible_zt
-sudo /bin/chmod 0644 /etc/sudoers.d/ansible_zt
-sudo /bin/rm /etc/sudoers.d/ansible 2>/dev/null

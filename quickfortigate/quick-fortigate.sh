c9995c70a1187efd85793f3bcd7177b8d979c743
zhou <zkillian@fortinet.com>
2026-06-25 11:30:06 +0200

    Working QuickFortiGate

--- quickfortigate/quick-fortigate.sh
+++ quickfortigate/quick-fortigate.sh
@@ -6,12 +6,12 @@ ansible-vault encrypt --vault-password-file ./src/vars/secret.pass ./src/vars/co
 # ─────────────────────────────────────────────
 # Initialise proxmox
 # ─────────────────────────────────────────────
-ansible-playbook --vault-password-file ./src/vars/secret.pass -i ./src/vars/inventory.yml ./src/setup_fortigate.yml
+OBJC_DISABLE_INITIALIZE_FORK_SAFETY=yes ansible-playbook --vault-password-file ./src/vars/secret.pass -i ./src/vars/inventory.yml ./src/setup_fortigate.yml
 
 # ─────────────────────────────────────────────
 # Post installation setup
 # ─────────────────────────────────────────────
-ansible-playbook --vault-password-file ./src/vars/secret.pass -i ./src/vars/temp_inventory.yml ./src/post_install.yml
+OBJC_DISABLE_INITIALIZE_FORK_SAFETY=yes ansible-playbook --vault-password-file ./src/vars/secret.pass -i ./src/vars/temp_inventory.yml ./src/post_install.yml
 
 # ─────────────────────────────────────────────
 # Cleaning
